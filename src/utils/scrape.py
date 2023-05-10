e616c3872058619ce7d34a95a837e5e07a9c9214
Bentley Hensel <bentley.hensel@civicactions.com>
2023-05-10 00:08:29 +0000

    Remove baddies from queue

--- src/utils/scrape.py
+++ src/utils/scrape.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from utils.watch import logger
 from utils.auth import rabbit
+from utils.health import test_proxy
 
 
 def clean_url(url):
@@ -91,7 +92,7 @@ def process_message(channel, method, properties, body):
     except requests.exceptions.Timeout as e:
         error_message = f"❌ Failed to process {url}: Request timed out. {e}"
         logger.error(error_message)
-        time.sleep(15)  # Pause for 15 seconds
+        time.sleep(1)  # Pause for 1 second
         channel.basic_nack(delivery_tag=method.delivery_tag)
 
         # Send a message to the error_crawler queue
@@ -101,12 +102,28 @@ def process_message(channel, method, properties, body):
             "error_message": error_message
         })
         send_to_queue("error_crawler", error_payload)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    # Proxy Exceptions
+    except requests.exceptions.ProxyError as e:
+        error_message = f"❌ Failed to process {url}: Proxy error. {e}"
+        logger.error(error_message)
+
+        if not test_proxy():
+            error_payload = json.dumps({
+                "url_id": url_id,
+                "url": url,
+                "error_message": error_message
+            })
+            send_to_queue("error_crawler", error_payload)
+
+        time.sleep(1)  # Pause for 15 seconds
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+    # Other exceptions
     except Exception as e:
         error_message = f"❌ Failed to process {url}: {e}"
         logger.error(error_message)
-        time.sleep(15)  # Pause for 15 seconds
-        channel.basic_nack(delivery_tag=method.delivery_tag)
-
+        time.sleep(1)  # Pause for 15 seconds
+        channel.basic_ack(delivery_tag=method.delivery_tag)
         # Send a message to the error_crawler queue
         error_payload = json.dumps({
             "url_id": url_id,
